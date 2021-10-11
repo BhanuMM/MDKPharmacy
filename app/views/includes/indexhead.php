db293fb06df93eec1e811861eb66c9cd6a7e4006
Heshan1999 <heshanweerasinghe99@gmail.com>
2021-10-11 10:16:32 +0530

    Landing Page added

---
+++ app/views/includes/indexhead.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MDK Pharmacy</title>
+    <!--Google Fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/landingstyle.css">
+
+
+</head>
+<body>
+    <section class="header"> 
+        <!--top bar ( time/contact) -->
+        <div class="top-header">
+            <div class="text-left">
+                <p>Opening Hours: 6.00 Am - 11.00 Pm (Mon - Sat)</p>
+            </div>
+            <div class="text-right">
+                Call Us At +94 011 752 8659
+            </div>
+        </div>
+        <!--logo and menu-->
+        <nav>
+            <div class="logo">
+                <a href = "index.html"><img src="<?php echo URLROOT ?>/public/images/logo1.png"> </a>
+            </div>
+            
+        <div class="nav-links" id="navLinks">
+
+        <!--font awesome for closing-->
+        <!-- <i class="fas fa-times" onclick="hideMenu()"></i> -->
+
+            <div class="nav-align">
+
+           
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Facilities</a></li>
+                <li><a href="">Pharmacy</a></li>
+                <li><a href="">Surgeries</a></li>
+                <li><a href="">Services</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="<?php echo URLROOT; ?>/users/login">Log In</a></li>
+            </ul>
+            
+          </div>
+        </div> 
+
+        <!--font awesome for bars-->
+        <i class="fas fa-bars" onclick="showMenu()"></i>
+
+
+    </nav>
+
+    </section>
--- app/views/index.php
+++ app/views/index.php
@@ -1,13 +1,120 @@
 <?php
-   require APPROOT . '/views/includes/head.php';
+   require APPROOT . '/views/includes/indexhead.php';
 ?>
-<div id="section-landing">
-    <!-- ?php
-       require APPROOT . '/views/includes/navigation.php';
-    ?> -->
 
-    <div class="wrapper-landing">
-        <h1>MDK HOSPITALS</h1>
-        <h2>landing Page</h2>
+
+
+    <!-- About Us -->
+
+    <section class="about">
+        <div class="about-left">
+            <h1>About Us</h1><br><br>
+            <h2>Ensuring the Best in the Industry</h2><br>
+            <p>MDK Hospitals is the most accredited hospital in the Sri Lankan<br> healthcare sector. Since 2002, Lanka Hospitals has revolutionized<br> the healthcare industry through infrastructure development <br>and advancement of products and services, with a view to deliver<br> healthcare that is on par with global medical standards.
+            </p>
+
+
+        </div>
+
+        <div class="about-right">
+            <img src="<?php echo URLROOT ?>/public/images/c1.jpg">
+        </div>
+
+
+
+
+
+    </section>
+
+
+    
+    <section class="op">
+        <div class="op-right">
+            <h1>Online Pharmacy</h1><br><br>
+            <h2>Order your medicine to the doorsteps during <br>the pandemic</h2><br>
+            <p>MDK Hospitals is the most accredited hospital in the Sri Lankan<br> healthcare sector. Since 2002, Lanka Hospitals has revolutionized<br> the healthcare industry through infrastructure development <br>and advancement of products and services, with a view to deliver<br> healthcare that is on par with global medical standards.<br><br>
+
+                <button class="button"><span>Visit now</span></button>
+            </p>
+            
+
+
+        </div>
+
+        <div class="op-left">
+            <img src="<?php echo URLROOT ?>/public/images/c2.jpg">
+        </div>
+
+    </section>
+
+
+    <!-- facilities -->
+    <section class= "fac">
+    <h1>Our Facilities</h1>
+    <p>Lorem ipsum dolor sit amet consectetur <br> adipisicing elit. Incidunt placeat perferendis, quaerat earum rem excepturi <br> accusantium delectus magni omnis laboriosam quia nihil velit illum quos tempora at ea qui itaque!
+    </p>
+    <div class="row">
+    <div class="fac-col">
+        <h3>Surgery</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, explicabo quia incidunt omnis labore cum est assumenda temporibus provident neque reprehenderit sunt, qui reiciendis quaerat exercitationem mollitia pariatur hic. Quo!</p>
     </div>
-</div>
+    <div class="fac-col">
+        <h3>Dental</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, explicabo quia incidunt omnis labore cum est assumenda temporibus provident neque reprehenderit sunt, qui reiciendis quaerat exercitationem mollitia pariatur hic. Quo!</p>
+    </div>
+    <div class="fac-col">
+        <h3>Pharmacy</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, explicabo quia incidunt omnis labore cum est assumenda temporibus provident neque reprehenderit sunt, qui reiciendis quaerat exercitationem mollitia pariatur hic. Quo!</p>
+    </div>
+    </div>
+
+
+    </section>
+
+    
+    <!-- facilities -->
+    <section class= "footer">
+        <div class="footer-row">
+            <h1>MDK Hospitals</h1>
+            <p>committed to provide compassionate care <br>and excellent service that transcends <br>conventional healthcare.
+            </p>
+            <!-- <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Facilities</a></li>
+                <li><a href="">Pharmacy</a></li>
+                <li><a href="">Surgeries</a></li>
+                <li><a href="">Services</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">History</a></li>
+            </ul> -->
+        </div>
+       
+    
+    
+    </section>
+    
+    
+    
+
+
+
+    <!-- JavaScript-->
+<!-- 
+    <script>
+        window.onscroll = function() {myFunction()};
+        
+        var navbar = document.getElementById("navbar");
+        var sticky = navbar.offsetTop;
+        
+        function myFunction() {
+          if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+          } else {
+            navbar.classList.remove("sticky");
+          }
+        }
+    </script> -->
+
+    
+</body>
+</html>
