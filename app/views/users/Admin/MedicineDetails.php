22971a8467ac76fc967f653975eca69bf4727ccf
shanika-96 <87492020+shanika-96@users.noreply.github.com>
2021-10-14 23:41:02 +0530

    Medicine details table added

--- app/views/users/Admin/MedicineDetails.php
+++ app/views/users/Admin/MedicineDetails.php
@@ -3,21 +3,22 @@ require APPROOT . '/views/includes/Adminhead.php';
 ?>
 
 <div style="margin-left:17%; margin-right:2%; padding:1px 16px; width: ">
-                <a href="../Admin/AddStock.html"><button style="margin-top: 10%;" class="button button1">Add New Stock +</button></a>
+                <a href="../Admin/AddStock.html"><button style="margin-top: 10%;" class="button button1">Add New Medicine +</button></a>
                 <ul style="padding-left: 0px; list-style-type: none; overflow: auto;">
-                    <li Style="float: left; vertical-align: middle; display: inline;"><h3> Stock Details</h3></li>
+                    <li Style="float: left; vertical-align: middle; display: inline;"><h3> Medicine Details</h3></li>
                     <li Style="float: right; padding-left: 1%; vertical-align: middle; display: inline;"><img src="<?php echo URLROOT ?>/public/images/Search.png" alt="Search" height="15px" style="opacity: 0.5;"></li>
                     <li Style="float: right; vertical-align: middle; display: inline;"><input type="text" placeholder="Search Stock"></li>
                 </ul>
                 
                 <table id="customers">
                     <tr>
-                      <th>Item Code</th>
-                      <th>Quantity</th>
-                      <th>Purchase unit price</th>
-                      <th>Selling unit price</th>
-                      <th>Purchase Date</th>
-                      <th>Expiry Date</th>
+                      <th>Generic Name</th>
+                      <th>Brand Name</th>
+                      <th>Importer Name</th>
+                      <th>Dealer</th>
+                      <th>Purchase Price</th>
+                      <th>Selling Price</th>
+                      <th>Profit Margin</th>
                       <th>Update</th>
                       <th>Delete</th>
                     </tr>
@@ -30,18 +31,21 @@ require APPROOT . '/views/includes/Adminhead.php';
                       <td></td>
                       <td></td>
                       <td></td>
-                    </tr>
-                    <tr>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
                       <td></td>
                     </tr>
                     <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
                         <td></td>
                         <td></td>
                         <td></td>
