3ec685c29b09a484908b3e751250f0b804d16de4
MRavindu <68803688+MRavindu@users.noreply.github.com>
2021-10-15 12:26:20 +0530

    Connected the Counsellor Pages

--- app/views/includes/Counsellorhead.php
+++ app/views/includes/Counsellorhead.php
@@ -14,24 +14,33 @@
 <div class="sidebar">
     <header>MDK HOSPITALS</header>
     <ul style="list-style-type: none; padding-left: 0px;">
-        <li><a href = "../Admin/AdminDashboard.html">Dashboard</a></li>
-        <li><a href = "../Admin/UserDetails.html"> Users</a></li>
-        <li><a href = "../Admin/ReportDetails.html">Reports </a></li>
-        <li><a href = "../Admin/SupplierDetails.html">Suppliers</a></li>
-        <li><a href = "../Admin/StockDetails.html">Stocks</a></li>
+        <li><a href = "<?php echo URLROOT ?>/counsellors/counsellordashboard">Dashboard</a></li>
+        <li><a href = "<?php echo URLROOT ?>/counsellors/seemedicineavailability">Medicine Availability</a></li>
+<!--        <li><a href = "../Admin/ReportDetails.html">Reports </a></li>-->
+<!--        <li><a href = "../Admin/SupplierDetails.html">Suppliers</a></li>-->
+<!--        <li><a href = "../Admin/StockDetails.html">Stocks</a></li>-->
     </ul>
 </div>
 
 
 <div style="box-shadow: 1px 1px 5px #888888; background-color: #ffffff; padding-left: 10%; padding-top:0px; padding-bottom: 0px; height: 4.7%; ">
     <ul id="list2">
+        <a href = "<?php echo URLROOT ?>/counsellors/counsellordashboard">
         <li style="padding-left: 4%; padding-right: 60%; float: left;"><button class="button"><img src="<?php echo URLROOT ?>/public/images/Back.png" alt="Return to Dashboard" height="15px" style="opacity: 0.5;">Back to Dashboard</button></li>
-        <li><img src="<?php echo URLROOT ?>/public/images/Settings.png" alt="Settings" height="15px" style="opacity: 0.5;"></li>
-        <li><img src="<?php echo URLROOT ?>/public/images/Notification.png" alt="Notifications" height="15px" style="opacity: 0.5;"></li>
-        <li><img src="<?php echo URLROOT ?>/public/images/Message.png" alt="messages" height="15px" style="opacity: 0.5;"></li>
+        </a>
+<!--        <li><img src="--><?php //echo URLROOT ?><!--/public/images/Settings.png" alt="Settings" height="15px" style="opacity: 0.5;"></li>-->
+<!--        <li><img src="--><?php //echo URLROOT ?><!--/public/images/Notification.png" alt="Notifications" height="15px" style="opacity: 0.5;"></li>-->
+<!--        <li><img src="--><?php //echo URLROOT ?><!--/public/images/Message.png" alt="messages" height="15px" style="opacity: 0.5;"></li>-->
         <li style="color: #afafaf;"> | </li>
-        <li style="color: #888888;">UCSC</li>
-        <li><img src="<?php echo URLROOT ?>/public/images/Drop.png" alt="View" height="8px" style="opacity: 0.5;"> </li>
+        <li style="color: #888888;"><?php echo $_SESSION['username'] ?></li>
+        <li><div class="dropdown">
+                <span> <img src="<?php echo URLROOT ?>/public/images/Drop.png" alt="View" height="8px" style="opacity: 0.5;"></span>
+                <div class="dropdown-content">
+                    <p><button class="btn-ddc">Settings</button></p>
+                    <p><button class="btn-ddc">Logout</button></p>
+                </div>
+            </div>
+        </li>
         <li><img src="<?php echo URLROOT ?>/public/images/Profile.png" alt="Profile" height="35px" style="opacity: 0.5;"></li>
 
     </ul>
