8253317ead7ad54d4387f2620f9b1b6c1026d661
MRavindu <68803688+MRavindu@users.noreply.github.com>
2021-10-23 00:10:58 +0530

    updated CSS of the Counsellor search bars

--- app/views/users/Delivery/PastDeliveries.php
+++ app/views/users/Delivery/PastDeliveries.php
@@ -4,8 +4,10 @@ require APPROOT . '/views/includes/Deliveryhead.php';
 <div style="margin-left: 300px; margin-top:50px; margin-right:0%; padding:1px 16px; width: 70%; ">
                 <ul style="padding-left: 0px; list-style-type: none; overflow: auto;">
                     <li Style="float: left; vertical-align: middle; display: inline;"><h3> Past Deliveries</h3></li>
-                    <li Style="float: right; padding-left: 1%; vertical-align: middle; display: inline;"><img src="<?php echo URLROOT ?>/public/images/Search.png" alt="Search" height="15px" style="opacity: 0.5;"></li>
-                    <li Style="float: right; vertical-align: middle; display: inline;"><input type="text" placeholder="Search Deliveries"></li>
+                    <form><li Style="float: right; padding-left: 1%; vertical-align: middle; display: inline;">
+                            <a style="border-left: 0px solid !important" href="#"><img src="<?php echo URLROOT ?>/public/images/Search.png" alt="Search" style="opacity: 0.5; height: 25px; margin-top: 8px; position:relative; margin-right: 10px; "></a></li>
+                        <li Style="float: right; vertical-align: middle; display: inline;">
+                            <input type="text" id="UISearchbar" style="height: 35px;" placeholder="Search Deliveries"></li></form>
                 </ul>
                 
                 <table id="customers">
