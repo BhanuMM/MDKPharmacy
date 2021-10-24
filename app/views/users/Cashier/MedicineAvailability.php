a3762de3baa97008fa475b16f1095ec9caca9990
Heshan1999 <heshanweerasinghe99@gmail.com>
2021-10-24 13:29:25 +0530

    pharmacist scroll bar removed

--- app/views/users/Cashier/MedicineAvailability.php
+++ app/views/users/Cashier/MedicineAvailability.php
@@ -2,7 +2,7 @@
 require APPROOT . '/views/includes/Cashierhead.php';
 ?>
             <div style="margin-left: 300px; margin-top:50px; margin-right:0%; padding:1px 16px; width: 70%;">
-                <ul style="margin-top: 5%; padding-left: 0px; padding-top: 2%; list-style-type: none; overflow: auto;">
+                <ul style="margin-top: 5%; padding-left: 0px; padding-top: 2%; list-style-type: none; ">
                     <li Style="float: left; vertical-align: middle; display: inline;"><h3>Medicine Details</h3></li>
                     <form><li Style="float: right; padding-left: 1%; vertical-align: middle; display: inline;">
                             <a style="border-left: 0px solid !important" href="#"><img src="<?php echo URLROOT ?>/public/images/Search.png" alt="Search" style="opacity: 0.5; height: 25px; margin-top: 8px; position:relative; margin-right: 10px; "></a></li>
--- app/views/users/Pharmacist/PrescriptionDetails.php
+++ app/views/users/Pharmacist/PrescriptionDetails.php
@@ -13,7 +13,7 @@ require APPROOT . '/views/includes/Pharmacisthead.php';
         </div>
 
 
-    <ul style="margin-top: 5%; padding-left: 0px; padding-top: 2%; list-style-type: none; overflow: auto;">
+    <ul style="margin-top: 5%; padding-left: 0px; padding-top: 2%; list-style-type: none; ">
         <li Style="float: left; vertical-align: middle; display: inline;"><h3>Prescriptions</h3></li>
         <form><li Style="float: right; padding-left: 1%; vertical-align: middle; display: inline;">
                 <a style="border-left: 0px solid !important" href="#"><img src="<?php echo URLROOT ?>/public/images/Search.png" alt="Search" style="opacity: 0.5; height: 25px; margin-top: 8px; position:relative; margin-right: 10px; "></a></li>
