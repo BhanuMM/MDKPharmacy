57ef302a987e8d354cb39e3b3066305694cb22af
Heshan1999 <heshanweerasinghe99@gmail.com>
2021-10-24 11:16:19 +0530

    index head updated

--- app/views/includes/indexhead.php
+++ app/views/includes/indexhead.php
@@ -22,7 +22,7 @@
         <!--top bar ( time/contact) -->
         <div class="top-header">
             <div class="text-left">
-                <p>Opening Hours: 6.00 Am - 11.00 Pm (Mon - Sat)</p>
+                <p>Open 24 hours</p>
             </div>
             <div class="text-right">
                 Call Us At +0347 888 888
