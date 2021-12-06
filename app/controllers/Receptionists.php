94eee1c14d4a03dad7fbf6b6299974a7d94a5fa7
shanika-96 <87492020+shanika-96@users.noreply.github.com>
2021-12-06 22:39:03 +0530

    update/delete patient

--- app/controllers/Receptionists.php
+++ app/controllers/Receptionists.php
@@ -93,4 +93,108 @@ class Receptionists extends Controller
     {
         $this->view('users/Receptionist/ReceptionistProfileSetting');
     }
+
+    
+
+    
+    
+    public function updatepatient($patid)
+    {
+        $pat = $this->receptionistModel->findPatientById($patid);
+
+        $data = [
+            'patientid' => $pat->patid,
+            'patientname' => $pat->patname,
+            'patientnic' => $pat->patnic,
+            'patientadrs' => $pat->patadrs,
+            'patientdob' => $pat->patdob,
+            'patientgen' => $pat->patgen,
+            'patienttelno' => $pat->pattelno,
+            'patientemail' => $pat->patemail,
+            'nicError' => '',
+            'telError' => '',
+            'nameError' => ''
+
+        ];
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'patientid' => $pat->patid,
+                'patientname' => trim($_POST['patname']),
+                'patientnic' => trim($_POST['patnic']),
+                'patientadrs' => trim($_POST['patadrs']),
+                'patienttelno' => trim($_POST['pattelno']),
+                'patientemail' => trim($_POST['patemail']),
+                'patientdob' => $_POST['patdob'],
+                'patientgen' => $_POST['patgen']
+            ];
+
+            // Validate nic on length, numeric values,
+            $nicValidation = "/^([0-9]{9}[x|X|v|V]|[0-9]{12})$/";
+            if( preg_match($nicValidation, $data['patientnic']) ){
+                $data['nicError'] = '';
+            }else{
+                $data['nicError'] = 'Invalid NIC Number';
+            }
+
+
+            // Validate telephone on length, numeric values,
+            $telValidation = "/^[0-9]+$/";
+            if(strlen($data['patienttelno']) ==10 && preg_match($telValidation, $data['patienttelno']) ){
+                $data['telError'] = '';
+            }else{
+                $data['telError'] = 'Invalid Contact Number';
+            }
+
+            // Make sure that errors are empty
+            if (empty($data['nameError']) && empty($data['telError']) && empty($data['nicError'])) {
+
+
+                //Register user from model function
+                if ($this->receptionistModel->updatepatient($data)) {
+                    //Redirect to the viewtable page
+                    $recadded = ' Patient details has been Successfully Updated!';
+                    header('location: ' . URLROOT . '/receptionists/viewpatients?msg='.$recadded);
+                } else {
+                    die('Something went wrong.');
+                }
+            }
+            
+        }
+        $this->view('users/Receptionist/UpdatePatient',$data);
+    }
+
+
+    public function deletepatient($patid){
+        $pat = $this->receptionistModel->findPatientById($patid);
+
+        $data = [
+            'patientid' => $pat->patid,
+            'patientname' => $pat->patname,
+            'patientnic' => $pat->patnic,
+            'patientadrs' => $pat->patadrs,
+            'patientdob' => $pat->patdob,
+            'patientgen' => $pat->patgen,
+            'patienttelno' => $pat->pattelno,
+            'patientemail' => $pat->patemail,
+            'nicError' => '',
+            'telError' => '',
+            'nameError' => ''
+
+        ];
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+        if ($this->receptionistModel->deletepatient($data)) {
+         //Redirect to the viewtable page
+        $recadded = ' Patient details has been Deleted!';
+        header('location: ' . URLROOT . '/receptionists/viewpatients');
+         } else {
+        die('Something went wrong.');
+            }
+    }
+}
 }
